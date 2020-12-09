473b1482876513e2f3f46602dcdf2616adf6fab6
lerochas <emaildole.07@gmail.com>
2020-12-09 14:05:05 -0300

    adding balance graph

--- src/components/BalancePanel/BalancePanelChart/index.js
+++ src/components/BalancePanel/BalancePanelChart/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
+import {BarChart} from 'react-native-svg-charts';
+
 const BalancePanelChart = () => {
-  return <View />;
+  const data = [100, 80, -30, 110, 25, 35, 70];
+  return (
+    <View style={styles.container}>
+      <BarChart
+        style={styles.chart}
+        data={data}
+        svg={{
+          fill: 'rgba(0,0,0,.1)',
+          stroke: 'rgba(0,0,0,.1)',
+          strokeWidth: 1,
+        }}
+        contentInset={{top: 0, bottom: 0}}
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 50,
+    marginTop: -20,
+    marginBottom: 3,
+  },
+  chart: {
+    height: 70,
+  },
+});
 
 export default BalancePanelChart;
