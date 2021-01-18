02cc9b2c42779836667324cc9d1d764e8eca95dd
lerochas <emaildole.07@gmail.com>
2021-01-18 19:33:14 -0300

    Integrating with dynamic route systems

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -10,6 +10,7 @@ import WelcomeBalanceInput from './WelcomeBalanceInput';
 
 import useCategories from '../../hooks/useCategories';
 import {saveEntry} from '../../services/Entries';
+import {setInitialized} from '../../services/Welcome';
 
 import Colors from '../../styles/Colors';
 import Logo from '../../assets/logo-white.png';
@@ -25,6 +26,7 @@ const Welcome = ({navigation}) => {
       category: initCategories,
     });
 
+    setInitialized();
     navigation.navigate('Main');
   };
 
